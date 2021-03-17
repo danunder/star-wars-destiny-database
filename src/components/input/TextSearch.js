24b62981c63b06a8539d4cd180398b6e5beaad6f
Daniel Robertson <danieljrobertson@gmail.com>
2021-03-17 15:20:26 -0400

    create TextSearch component

---
+++ src/components/input/TextSearch.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { setCardText } from "../../redux/actions/setActions";
+import { useSelector, useDispatch } from "react-redux";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faTimes} from '@fortawesome/free-solid-svg-icons';
+
+function TextSearch(props) {
+
+
+    const style =useSelector(state=>state.style);
+
+    const cardText =useSelector(state=>state.cardText);
+    const dispatch = useDispatch();
+
+    const handleChange = event => {
+        dispatch(setCardText(event.target.value));
+    };
+
+
+    let sty = {
+        backgroundColor: style.navText,
+        color: style.nav,
+
+    };
+
+    let sg = {
+        color: style.nav
+    };
+
+    let close = (cardText!==""?<FontAwesomeIcon style={sg} icon={faTimes} onClick={()=>dispatch(setCardText(""))}/>:null);
+
+    return (
+        <div className={"searchContainer"}>
+            <input style={sty} placeholder={"Search Card by card text..."} value={cardText} onChange={handleChange}
+                   className={"search "+style.name}/>
+            {close}
+        </div>
+
+    );
+}
+
+
+
+
+
+export default Search;
