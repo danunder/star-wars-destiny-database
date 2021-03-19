5d2d103109e964eed00e3f804d8bbb58fc573c94
Daniel Robertson <danieljrobertson@gmail.com>
2021-03-19 10:50:22 -0400

    begin implementing variable search box

--- src/components/input/TextSearch.js
+++ src/components/input/TextSearch.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import { setCardText } from "../../redux/actions/setActions";
+import React, {useState} from 'react';
+import { setName, setCardText } from "../../redux/actions/setActions";
 import { useSelector, useDispatch } from "react-redux";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faTimes} from '@fortawesome/free-solid-svg-icons';
 
 function TextSearch(props) {
 
+    // we don't do this in REDUX. no local state right? DO SOME READING.
+    const { searchType, setSearchType } = useState("card name")
 
     const style =useSelector(state=>state.style);
 
-    const cardText =useSelector(state=>state.cardText);
+    const cardText = useSelector(state => state.cardText);
+    const name = useSelector(state=>state.name);
     const dispatch = useDispatch();
 
     const handleChange = event => {
@@ -27,11 +30,13 @@ function TextSearch(props) {
         color: style.nav
     };
     
+    
+
     let close = (cardText!==""?<FontAwesomeIcon style={sg} icon={faTimes} onClick={()=>dispatch(setCardText(""))}/>:null);
 
     return (
         <div className={"searchContainer"}>
-            <input style={sty} placeholder={"Search Card by Card Text..."} value={cardText} onChange={handleChange}
+            <input style={sty} placeholder={`Search Card by ${searchType}...`} value={cardText} onChange={handleChange}
                    className={"search "+style.name}/>
             {close}
         </div>
